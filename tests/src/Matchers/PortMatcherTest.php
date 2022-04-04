5a92b28b5f3091b8975cedfd337843f2ca4f4bf9
Neur0toxine <pashok9825@gmail.com>
2022-04-04 16:24:30 +0300

    phpstan baseline & better tests for the port matcher

--- tests/src/Matchers/PortMatcherTest.php
+++ tests/src/Matchers/PortMatcherTest.php
@@ -9,7 +9,8 @@
 
 namespace Pock\Tests\Matchers;
 
-use Pock\Matchers\HostMatcher;
+use Nyholm\Psr7\Uri;
+use Pock\Enum\RequestScheme;
 use Pock\Matchers\PortMatcher;
 use Pock\TestUtils\PockTestCase;
 
@@ -29,5 +30,31 @@ class PortMatcherTest extends PockTestCase
     public function testMatches(): void
     {
         self::assertTrue((new PortMatcher(443))->matches(static::getTestRequest()));
+    }
+
+    public function testMatchesWithoutProto(): void
+    {
+        self::assertTrue((new PortMatcher(80))->matches(static::getTestRequest()->withUri(new class extends Uri {
+            public function getScheme(): string
+            {
+                return RequestScheme::HTTP;
+            }
+
+            public function getPort(): ?int
+            {
+                return null;
+            }
+        })));
+        self::assertTrue((new PortMatcher(443))->matches(static::getTestRequest()->withUri(new class extends Uri {
+            public function getScheme(): string
+            {
+                return RequestScheme::HTTPS;
+            }
+
+            public function getPort(): ?int
+            {
+                return null;
+            }
+        })));
     }
 }
